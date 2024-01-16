38079f0bf59f16fa41b0caee913809965eabac39
Merzoit <68446392+Merzoit@users.noreply.github.com>
2024-01-17 00:19:06 +0300

    reload version

--- core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-urlpatterns = [
-	path('', MainView.as_view(), name='main'),
-	path('menu/', MenuView.as_view(), name='menu'),
-	path('db/league/', LeagueView.as_view(), name='league_list'),
-	path('db/league/<int:league_id>', LeagueDetailView.as_view(), name='league_details')
-]
